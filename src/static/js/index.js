6e1550d913ce08bdc4cff377f7485d0ebbbbc4e2
dimonos33ru <dimonos05psy@gmail.com>
2019-06-15 12:56:43 +0300

    Переделал структуру проекта и слайдер.

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -5,8 +5,15 @@ $(document).ready(() => {
 	$('#slider-main').owlCarousel({
 		items: 1,
 		autoplay: true,
+		autoplayTimeout: 8000,
+		nav: true,
 		dots: false,
 		loop: true,
 		smartSpeed: 1000,
+		animateOut: 'fadeOut',
+
+		navText: ['<i class="fas fa-chevron-circle-left"></i>', '<i class="fas fa-chevron-circle-right"></i>'],
+		navContainerClass: 'slider-main__nav',
+		navClass: ['slider-main__arrow-left', 'slider-main__arrow-right'],
 	});
 });
