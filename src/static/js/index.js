babf61eaa5b3c8245ea09760ccf281cd442f51d2
dimonos33ru <dimonos05psy@gmail.com>
2019-09-07 13:07:05 +0300

    Изменения дизайна(менею,слайдер в шапке, блок преимуществ), добавил шрифт Monseratt

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -34,7 +34,42 @@ $(document).ready(() => {
 	$('a[href^="#"]').on('click', function () {
 		$('html, body').animate({
 			scrollTop: $($.attr(this, 'href')).offset().top,
-		}, 1000);
+		}, 1600);
 		return false;
 	});
+
+	/* --------- Lazy Load YandexMap --------- */
+	let yaMapsShow = false;
+	const divYaMaps = document.getElementById('YaMaps');
+
+	if (isInPage(divYaMaps)) {		
+		$('a[href="#contacts"]').click(() => {
+			if (!yaMapsShow) {
+				showYaMaps();
+				yaMapsShow = true;
+			}
+		});
+
+		$(window).scroll(() => {
+			if (!yaMapsShow) {
+				if ($(window).scrollTop() + $(window).height() > $(document).height() - 1000) {
+					showYaMaps();
+					yaMapsShow = true;
+				}
+			}
+		});
+	}
+
+	// Функция проверяет, есть ли элемент на странице
+	function isInPage(node) {
+		return (node === document.body) ? false : document.body.contains(node);
+	}
+	// Создает элемент с Яндекс картой
+	function showYaMaps() {
+		const script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3e320335013fd0d266686d9f78e94b52d990fdcb8b12710571539f8fdd72b581&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=false';
+		document.getElementById('YaMaps').appendChild(script);
+	}
+	/* --------- end lazy load --------- */
 });
